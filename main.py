c44b48b126cd791a479ceaca5fd50d36f486f131
SalebaAbukhader <Salebaak@gmail.com>
2025-12-23 19:15:22 +0000

    Spam Email Detector using Scikit-learn

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
-#url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"
+#URL : https://www.kaggle.com/datasets/colormap/spambase
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-import seaborn as sns
+import pandas as pd 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model  import LogisticRegression 
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score , confusion_matrix 
 import matplotlib.pyplot as plt
+import seaborn as sns 
 
-# Load Dataset
-url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"
-df = pd.read_csv(url)
+# Load the dataset 
 
-# Define features and target
-features = df[['total_bill', 'size']]
-target = df['tip']
+data = pd.read_csv('spambase.csv')
+X =  data.drop('spam', axis=1)
+y = data['spam']
 
-print("Features: \n", features.head())
-print("Target: \n", target.head())
+# Split the data into training and testing sets 
 
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+X_train , X_test , y_train , y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-print("Training Data Set: ", X_train.shape)
-print("Testing Data Set: ", X_test.shape)
+# Train a logistic regression model
 
-# Visualize relationships
-sns.pairplot(df, x_vars=["total_bill", "size"], y_vars="tip", height=5, aspect=0.8, kind="scatter")
-plt.suptitle("Feature vs Target Relationships")
-plt.savefig("plot.png")
-print("Plot saved to plot.png")
+model = LogisticRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print(X_test)
+
+
+# Evaluate the model 
+accuracy  = accuracy_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+
+print(f"Accuracy: {accuracy}")
+print(f"F1 Score: {f1}")
+print(f"Precision: {precision}")
+print(f"Recall: {recall}")
+
+# Confusion Matrix (visualization))
+
+cm = confusion_matrix(y_test, y_pred)
+sns.heatmap(cm, annot=True , fmt='d')
+plt.title('Confusion Matrix')
+plt.show()
+
+
+
